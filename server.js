c6c9242c74afa3695c5428b410cb19816136b8a9
Tezore <tezore@web.de>
2022-05-02 17:39:18 +0200

    Created server.js
    Main script for Node.js server

---
+++ server.js
@@ -0,0 +1,53 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const server = require("http").createServer(app);
+const io = require("socket.io")(server);
+
+const mysql = require('mysql');
+const mysql_connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'test123'
+});
+
+// CONNECT TO MYSQL
+mysql_connection.connect(function(error){
+    if (!!error){
+        console.log("MySQL COULD NOT CONNECT");
+    } else {
+        console.log("MySQL CONNECTED");
+        // SERVER START
+        server.listen(process.env.PORT || 3000, () => {
+        console.log("Server started on Port 3000");
+        });
+    }
+});
+
+// ROUTES API REQUIRE
+app.use('/assets', express.static('assets')); // STYLE AND SCRIPT ROUTE
+const routes_api = require('./api/routes.js');
+app.use(routes_api);
+
+
+const connectedUsers = []
+
+io.on('connection', function(socket){
+
+    console.log("NEUER USER CONNECTED!");
+    connectedUsers.push(socket);
+    console.log("ANZAHL USER: " + connectedUsers.length);
+    socket.on('disconnect', function(){
+        var i = connectedUsers.indexOf(socket);
+        connectedUsers.splice(i, 1);
+        console.log("USER DISCONNECTED")
+        console.log("ANZAHL USER: " + connectedUsers.length);
+    });
+
+
+    // MODULES
+    require('./api/usermanager.js')(io,socket,mysql_connection);
+
+
+});
